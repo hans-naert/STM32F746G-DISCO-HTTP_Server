f780dfdd495ad7eb4bdb9f445fb26c12e75ee9dc
Hans Naert <hans.naert@vives.be>
2023-10-24 12:23:27 +0200

    add checkbox, update at 3s interval

--- Web/script.js
+++ Web/script.js
@@ -1,1 +1,16 @@
 console.log("Hello from script.js");
+
+window.setInterval(
+    (async () => {
+        console.log("Hello from timer.js");
+        let response = await fetch("button.cgi");
+        let text = await response.text();
+        console.log(text);
+        let checkbox=document.getElementById("checkbox-id");
+        if(text=="checked") {
+            checkbox.setAttribute("checked", "");
+        }
+        else {
+            checkbox.removeAttribute("checked");
+        }
+    }),3000);
